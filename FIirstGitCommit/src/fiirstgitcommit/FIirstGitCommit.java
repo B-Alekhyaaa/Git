2522b33a6249fb3c8591f6701d0a21dea8a8027e
B-Alekhyaaa <b.alekhya85@gmail.com>
2024-01-24 19:51:37 -0600

    FirstGitCommit

---
+++ FIirstGitCommit/src/fiirstgitcommit/FIirstGitCommit.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package fiirstgitcommit;
+
+/**
+ *
+ * @author s571697
+ */
+public class FIirstGitCommit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        // TODO code application logic here
+    }
+    
+}
